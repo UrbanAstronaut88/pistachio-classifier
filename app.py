e38e69815cc36d4ae61b5482d35d7b541880e689
BondarenkoKirill <bku089@gmail.com>
2026-02-21 23:20:17 +0200

    latest ver. before deployment

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from PIL import Image
 from classifier import predict_image
-import os
+
 
 app = Flask(__name__)
 
--- classifier.py
+++ classifier.py
@@ -6,23 +6,23 @@ model = tf.keras.models.load_model("model/pistachio_model.h5")
 
 class_names = ["Kirmizi_Pistachio", "Siirt_Pistachio"]
 
+
 def prepare_image(image):
+    image = image.convert("RGB")
     image = image.resize((128, 128))
-    image = np.array(image)
-
-    if image.shape[-1] != 3:
-        image = np.stack((image,) * 3, axis=-1)
-
-    image = image / 255.0
+    image = np.array(image, dtype=np.float32)
     image = np.expand_dims(image, axis=0)
     return image
 
 
 def predict_image(image):
     processed_image = prepare_image(image)
-    prediction = model.predict(processed_image)
 
+    prediction = model.predict(processed_image)
     probability = float(prediction[0][0])
+
+    print(f"! DEBUG ! Raw prediction: {prediction}")
+    print(f"! DEBUG ! Probability: {probability}")
 
     if probability > 0.5:
         predicted_class = class_names[1]
